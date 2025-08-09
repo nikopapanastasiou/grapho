7d305c7103d9a773def192a4a7ac10a7b0b50da0
Nick Papanastasiou <nick.papanastasiou93@gmail.com>
2025-08-09 09:53:49 -0500

    add DDL catalog

--- main.go
+++ main.go
@@ -2,11 +2,33 @@ package main
 
 import (
 	"fmt"
-	"grapho/parser"
+	"grapho/catalog"
 )
 
 func main() {
-	stmts, errs := parser.NewParser("CREATE NODE A(id:int); CREATE NODE B(id:int);").ParseScript()
-	fmt.Println(stmts)
-	fmt.Println(errs)
+	// at server boot:
+	store, _ := catalog.NewFileStore("./meta/catalog")
+	reg, _ := catalog.Open(store)
+
+	// use in executor for DDL:
+	ev := catalog.DDLEvent{
+		Op: catalog.OpCreateNode,
+		Stmt: catalog.CreateNodePayload{
+			Name: "Person",
+			Fields: []catalog.FieldPayload{
+				{Name: "id", Type: catalog.TypeSpec{Base: catalog.BaseUUID}, PrimaryKey: true},
+				{Name: "email", Type: catalog.TypeSpec{Base: catalog.BaseString}, Unique: true},
+			},
+		},
+	}
+	newCat, err := reg.Apply(ev)
+	_ = newCat
+	_ = err
+
+	// reads (lock-free):
+	cur := reg.Current()
+	if nt, ok := cur.Nodes["Person"]; ok {
+		fmt.Println(nt)
+	}
+
 }
